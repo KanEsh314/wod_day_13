fd5d94179ffcc92d2639b9c38e49892d59d19243
KanEsh314 <kanesh314@gmail.com>
2017-08-09 16:05:58 +0800

    first commit

---
+++ Wod.js
@@ -0,0 +1,30 @@
+function gred(score)
+{
+	var rou;
+
+	for(var i = 0; i < score.length; i++)
+	{
+		var scin;
+		var spr;
+
+		if(score[i] >= 38)
+		{
+			spr = score[i];
+			//document.write(spr);
+			var a = spr+1;
+			var b = spr+2;
+			var res;
+			if(a%5==0)
+			{
+				res = a;
+				document.write(res+"<br>");
+			}
+			else if (b%5==0)
+			{
+				res = b;
+				document.write(res+"<br>");
+			}
+		}
+	}
+}
+gred([4,73,67,38,33]);
